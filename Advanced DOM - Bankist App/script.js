1ea3a809408e7700e22bb4323ecc5b106d979cc4
Sandeep <sandeepkumar250903@gmail.com>
2023-06-28 23:46:13 +0530

    learnt some DOM elements selector

--- Advanced DOM - Bankist App/script.js
+++ Advanced DOM - Bankist App/script.js
@@ -22,7 +22,7 @@ const closeModal = function () {
 btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 
 // for (let i = 0; i < btnsOpenModal.length; i++)
-//   btnsOpenModal[i].addEventListener('click', openModal); /// Imrovised the above statement
+//   btnsOpenModal[i].addEventListener('click', openModal); /// Imrovisedbove traditional loop with forEach loop
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -32,3 +32,24 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+
+/////////////////////////////////////////////////////////////////////////////
+/*
+*/
+// DOM Lectures
+
+console.log(document.documentElement); // Select the entire webpage
+console.log(document.head); // Select the head of webpage
+console.log(document.body); // Select the body of webpage
+
+document.querySelector('.header') // Select a single element with class 'header'
+const allSections = document.querySelectorAll('.section'); // select all elements with 'section' class
+console.log(allSections);
+
+document.getElementById('section--1') // Select element by Id where you won't have to define the class or Id selector
+
+const allButtons = document.getElementsByTagName('button'); // Select all elements of the specific tag name (in this case it's 'button')
+
+document.getElementsByClassName('btn'); // Similar to getElementsById where you didn't had to specify the Id selector, similarly you won't have to specify the class selector here as well
+
