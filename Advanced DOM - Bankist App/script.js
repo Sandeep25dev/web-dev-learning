255be09f6dd8353ca40b3df22480de80c13eec2f
Sandeep <sandeepkumar250903@gmail.com>
2023-07-03 16:42:39 +0530

    Added the smooth scroll transitions effect

--- Advanced DOM - Bankist App/script.js
+++ Advanced DOM - Bankist App/script.js
@@ -9,6 +9,11 @@ const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
+const nav = document.querySelector('.nav');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
 
 const openModal = function (e) {
   e.preventDefault();
@@ -76,10 +81,6 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
 
 /// Building a tabbed component ///
 
-const tabs = document.querySelectorAll('.operations__tab');
-const tabsContainer = document.querySelector('.operations__tab-container');
-const tabsContent = document.querySelectorAll('.operations__content');
-
 tabsContainer.addEventListener('click', function (e) {
   const clicked = e.target.closest('.operations__tab'); // IT will search for the closest parent element with the classname of .operations__tab in order to avoid selecting the span element in the button which will disrupt the function
 
@@ -100,6 +101,102 @@ tabsContainer.addEventListener('click', function (e) {
 
 })
 
+//////////////////// READ THE CODE BELOW DEEPLY /////////////////////////////
+
+// Menu fade animation
+const handleHover = function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+}
+
+// nav.addEventListener('mouseover', function (e) {
+//   handleHover(e, 0.5);
+// })
+
+// Another Method :- but for this you have to change the opacity variable to this keyword and also remove the opacity parameter from the function
+
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+
+// nav.addEventListener('mouseout', function (e) {
+//   handleHover(e, 1);
+//  }); 
+
+nav.addEventListener('mouseout', handleHover.bind(1));
+
+
+//// Sticky Navigation
+
+// const initialCoords = section1.getBoundingClientRect();
+
+// window.addEventListener('scroll', function () {
+//   if (this.window.scrollY > initialCoords.top) nav.classList.add('sticky');
+//   else nav.classList.remove('sticky')
+// })
+
+//// Sticky Navigation: Intersection Observer API
+
+// const obsCallback = function (entries, observer) {
+//   entries.forEach(entry => {
+//     console.log(entry);
+//   })
+// };
+
+// const obsOptions = {
+//   root: null,
+//   threshold: 0.1,
+// }
+
+// const observer = new IntersectionObserver(obsCallback,obsOptions);
+// observer.observe(section1);
+
+const header = document.querySelector('.header');
+const navHeight = nav.getBoundingClientRect().height;
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  // console.log(entry);
+
+  if (!entry.isIntersecting) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+}
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold:0,
+  rootMargin: `-${navHeight}px`,
+})
+
+headerObserver.observe(header);
+
+// Reveal Sections
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+}
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+  
+});
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+})
+
 /////////////////////////////////////////////////////////////////////////////
 
 // DOM Lectures
@@ -110,8 +207,8 @@ tabsContainer.addEventListener('click', function (e) {
 // console.log(document.head); // Select the head of webpage
 // console.log(document.body); // Select the body of webpage
 
-const header = document.querySelector('.header') // Select a single element with class 'header'
-const allSections = document.querySelectorAll('.section'); // select all elements with 'section' class
+// const header = document.querySelector('.header') // Select a single element with class 'header'
+// const allSections = document.querySelectorAll('.section'); // select all elements with 'section' class
 // console.log(allSections);
 
 document.getElementById('section--1') // Select element by Id where you won't have to define the class or Id selector
