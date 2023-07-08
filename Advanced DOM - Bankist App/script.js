11851ac0b0aa39a7210a0ec17b91e4930c838e26
Sandeep <sandeepkumar250903@gmail.com>
2023-07-08 15:41:51 +0530

    added a slider element on the website

--- Advanced DOM - Bankist App/script.js
+++ Advanced DOM - Bankist App/script.js
@@ -197,6 +197,95 @@ allSections.forEach(function (section) {
   section.classList.add('section--hidden');
 })
 
+//// Lazy Loading Images ////
+
+const imgTargets = document.querySelectorAll('img[data-src]');
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  //Replace src with data-src
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
+  observer.unobserve(entry.target);
+};
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '200px',
+});
+
+imgTargets.forEach(img=>imgObserver.observe(img))
+
+////// Slider ////////
+
+
+/////////// REVISE THIS CODE ////////////
+
+
+const slides = document.querySelectorAll('.slide');
+const slider = document.querySelector('.slider');
+const btnLeft = document.querySelector('.slider__btn--left');
+const btnRight = document.querySelector('.slider__btn--right');
+const dotContainer = document.querySelector('.dots');
+
+
+let curSlide = 0;
+const maxSlide = slides.length - 1;
+
+const createDots = function () {
+  slides.forEach(function (_, i) {
+    dotContainer.insertAdjacentHTML('beforeend', `<button class="dots__dot" data-slide="${i}"></button>`)
+  })
+};
+
+createDots();
+
+
+// slider.style.transform = 'scale(0.5)';
+slider.style.overflow = 'hidden';
+
+const goToSlide = function (slide) {
+  slides.forEach((s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`));
+}
+goToSlide(0);
+// Next slide
+
+const nextSlide = function () {
+  if (curSlide === maxSlide) {
+    curSlide = 0;
+  } else {
+    curSlide++
+  }
+
+  goToSlide(curSlide);
+   
+};
+
+const prevSlide = function () {
+  if (curSlide === 0) {
+    curSlide = maxSlide - 1;
+  } else {
+    curSlide--;
+  }
+  goToSlide(curSlide);
+};
+
+btnRight.addEventListener('click', nextSlide);
+btnLeft.addEventListener('click', prevSlide);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'ArrowRight') nextSlide();
+  if (e.key === 'ArrowLeft') prevSlide();
+})
+
+
+
 /////////////////////////////////////////////////////////////////////////////
 
 // DOM Lectures
